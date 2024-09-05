094b296df6bc5fd58ec79f2b6bc1b9ca06482371
Misha <dudos0241@mail.ru>
2024-09-06 00:48:42 +0300

    Добавьте файлы проекта.

---
+++ AppDBContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OrderManagementAPI.Models;
+
+namespace OrderManagementAPI
+{
+    public class AppDBContext : DbContext
+    {
+        public AppDBContext(DbContextOptions<AppDBContext> options) : base(options)
+        {}
+
+        public DbSet<Order> Order { get; set; }
+    }
+}
---
+++ Models/Order.cs
@@ -0,0 +1,14 @@
+﻿namespace OrderManagementAPI.Models
+{
+    public class Order
+    {
+        public int OrderId { get; set; }
+        public string SenderCity { get; set; }
+        public string SenderAddress { get; set; }
+        public string ReceiverCity { get; set; }
+        public string ReceiverAddress { get; set; }
+        public decimal CargoWeight { get; set; }
+        public DateTime PickupDate { get; set; }
+        public string OrderNumber { get; set; }
+    }
+}
